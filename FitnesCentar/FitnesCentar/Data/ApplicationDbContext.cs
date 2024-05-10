c7c4d1274aa8dc1c9df9b042275753964eeb841a
Your Name <you@example.com>
2024-05-10 14:16:36 +0300

    Register/Login

---
+++ FitnesCentar/FitnesCentar/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnesCentar.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Client>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ProgramTask> ProgramTasks { get; set; }
+        public DbSet<FitnesInstructor> FitnesInstructors { get; set; }
+        public DbSet<ProgramCategory> ProgramCategories { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<ScheduleActivity> ScheduleActivities { get; set; }
+    }
+}
